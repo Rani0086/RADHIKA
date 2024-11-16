4a6b0cffb91f53e7ea2f621c86e6a3354acd8681
Rani0086 <shantidhaakad@gmail.com>
2024-11-16 06:18:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ API_ID = "16457832"
 # -------------------------------------------------------------
 API_HASH = "3030874d0befdb5d05597deacc3e83ab"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", "7638229482:AAHONy8JYrwzoCGNCzVcnADsjeRHjVOjbwY")
+BOT_TOKEN = getenv("BOT_TOKEN", "7153932073:AAE_YH8iy2Rf6p3gqEnyu0GGMqX-O8HYxUc")
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority")
-OWNER_ID = int(getenv("OWNER_ID", "7400383704"))
-SUPPORT_GRP = "+OL6jdTL7JAJjYzVl"
-UPDATE_CHNL = "BABY09_WORLD"
-OWNER_USERNAME = "UTTAM470"
+OWNER_ID = int(getenv("OWNER_ID", "7526369190"))
+SUPPORT_GRP = "waifexanime"
+UPDATE_CHNL = "Planetsadala"
+OWNER_USERNAME = "queen143np"
